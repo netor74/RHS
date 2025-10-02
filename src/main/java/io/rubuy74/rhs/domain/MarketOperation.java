f8000e312398bd24b4ca1eae32696c41cd75af56
Rubem Neto <rubemviscard.neto@blip.pt>
2025-10-02 11:40:06 +0100

    feat: separate json convertion & change toString()

--- src/main/java/io/rubuy74/rhs/domain/MarketOperation.java
+++ src/main/java/io/rubuy74/rhs/domain/MarketOperation.java
@@ -3,6 +3,7 @@ package io.rubuy74.rhs.domain;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.rubuy74.rhs.domain.http.MarketRequest;
 import io.rubuy74.rhs.domain.http.OperationType;
+import io.rubuy74.rhs.converter.EventDTOJSONConverter;
 
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -14,29 +15,17 @@ public class MarketOperation {
     @JsonProperty
     private OperationType operationType;
 
-    @SuppressWarnings("unchecked")
-    public static MarketOperation fromJson(LinkedHashMap<String, Object> rawPayload) {
-        MarketOperation marketOperation = new MarketOperation();
-        MarketRequest marketRequest = new MarketRequest();
-
-        Map<String, Object> marketRequestMap = (Map<String, Object>) rawPayload.get("marketRequest");
-        Map<String, Object> eventMap = (Map<String, Object>) marketRequestMap.get("event");
-
-        // add event to marketRequest
-        marketRequest.eventDTO = EventDTO.fromJson(eventMap);
-        marketRequest.marketId = (String)marketRequestMap.get("marketId");
-        marketRequest.marketName = (String)marketRequestMap.get("marketName");
-
-        if(marketRequestMap.containsKey("selections")){
-            List<Map<String, Object>> selectionsMap = (List<Map<String, Object>>) marketRequestMap.get("selections");
-
-            // add selections to marketRequest
-            marketRequest.selections = selectionsMap.stream().map(Selection::fromJson).toList();
-        }
-
-        marketOperation.marketRequest = marketRequest;
-        marketOperation.operationType = OperationType.valueOf((String) rawPayload.get("operationType"));
-        return marketOperation;
+    public MarketRequest getMarketRequest() {
+        return marketRequest;
+    }
+    public void setMarketRequest(MarketRequest marketRequest) {
+        this.marketRequest = marketRequest;
+    }
+    public OperationType getOperationType() {
+        return operationType;
+    }
+    public void setOperationType(OperationType operationType) {
+        this.operationType = operationType;
     }
 
     public MarketOperation() {}
