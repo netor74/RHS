9b610b91f99b8ebb3385061f4e343da97d138cd6
Rubem Neto <rubemviscard.neto@blip.pt>
2025-10-02 14:47:24 +0100

    feat: unit tests

--- src/main/java/io/rubuy74/rhs/dto/EventDTO.java
+++ src/main/java/io/rubuy74/rhs/dto/EventDTO.java
@@ -16,6 +16,27 @@ public class EventDTO {
 
     public EventDTO() {}
 
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getDate() {
+        return date;
+    }
+    
+    public void setDate(String date) {
+        this.date = date;
+    }
+
     public EventDTO(Event event) {
         this.id = event.getId();
         this.name = event.getName();
@@ -36,4 +57,5 @@ public class EventDTO {
                 .add("date",date)
                 .toString();
     }
+
 }
