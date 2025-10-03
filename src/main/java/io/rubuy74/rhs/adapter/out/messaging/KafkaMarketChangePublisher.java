75e54b1e6bf36c7fe4130bf6bbec51dba57b0e4a
Rubem Neto <rubemviscard.neto@blip.pt>
2025-10-03 10:08:07 +0100

    chore: fix date parsing

--- src/main/java/io/rubuy74/rhs/adapter/out/messaging/KafkaMarketChangePublisher.java
+++ src/main/java/io/rubuy74/rhs/adapter/out/messaging/KafkaMarketChangePublisher.java
@@ -15,10 +15,11 @@ import java.util.concurrent.CompletableFuture;
 public class KafkaMarketChangePublisher implements MarketChangePublisher {
     private static final Logger logger = LoggerFactory.getLogger(KafkaMarketChangePublisher.class);
     private static final String TOPIC = "market-changes";
-    private static final ObjectMapper mapper = new ObjectMapper() ;
+    private final ObjectMapper mapper ;
     private final KafkaTemplate<String, byte[]> kafkaTemplate;
 
-    public KafkaMarketChangePublisher(KafkaTemplate<String, byte[]> kafkaTemplate) {
+    public KafkaMarketChangePublisher(ObjectMapper objectMapper,KafkaTemplate<String, byte[]> kafkaTemplate) {
+        this.mapper = objectMapper;
         this.kafkaTemplate = kafkaTemplate;
     }
 
