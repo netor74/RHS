bc56372834b0cf2ce43e45a9637068ed639bea32
Rubem Neto <rubemviscard.neto@blip.pt>
2025-10-06 15:37:14 +0100

    chore: rearrange checks out of infrastructure layer

--- src/main/java/io/rubuy74/rhs/converter/SelectionJSONConverter.java
+++ src/main/java/io/rubuy74/rhs/converter/SelectionJSONConverter.java
@@ -2,26 +2,23 @@ package io.rubuy74.rhs.converter;
 
 import io.rubuy74.rhs.domain.Selection;
 import io.rubuy74.rhs.utils.ValidatorUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Map;
 
 public class SelectionJSONConverter {
-    private static final Logger logger = LoggerFactory.getLogger(SelectionJSONConverter.class);
+    private static final List<String> ATTRIBUTE_LIST = List.of("id","name","odd");
 
     public static Selection fromJson(Map<String,Object> rawPayload) {
-
-        List<String> attributeList = List.of("id","name","odd");
-        List<String> errorMessages = ValidatorUtils.checkAttributeList(rawPayload, attributeList);
-        if(!errorMessages.isEmpty()) {
-            errorMessages.forEach((errorMessage) -> {
-                logger.error("operation=deserialize_market_operation, msg={}", errorMessage);
-            });
-            return null;
-        }
-
+        ValidatorUtils.checkArgument(
+                rawPayload != null,
+                "Selection payload is null",
+                "deserialize_selection");
+        ValidatorUtils.checkAttributeList(
+                rawPayload,
+                ATTRIBUTE_LIST,
+                "deserialize_selection"
+                );
         String id = (String) rawPayload.get("id");
         String name = (String) rawPayload.get("name");
         Double odd = (Double) rawPayload.get("odd");
--- src/main/java/io/rubuy74/rhs/utils/ValidatorUtils.java
+++ src/main/java/io/rubuy74/rhs/utils/ValidatorUtils.java
@@ -1,27 +1,38 @@
 package io.rubuy74.rhs.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class ValidatorUtils {
-
-
-    public static List<String> checkAttributeList(
+    private static final Logger logger = LoggerFactory.getLogger(ValidatorUtils.class);
+    public static void checkAttributeList(
             Map<String,Object> rawPayload,
-            List<String> attributeList
+            List<String> attributeList,
+            String operation
     ) {
-        List<String> messages = new ArrayList<>();
+        List<String> errorMessages = new ArrayList<>();
         attributeList.forEach(attribute -> {
             if(!rawPayload.containsKey(attribute)) {
-                messages.add("attribute '" + attribute + "' doesn't exist");
+                errorMessages.add("attribute '" + attribute + "' doesn't exist");
             }
         });
-        return messages;
+
+        if(!errorMessages.isEmpty()) {
+            errorMessages.forEach((errorMessage) -> {
+                logger.error("operation={}, msg={}",operation, errorMessage);
+            });
+            throw new IllegalArgumentException(errorMessages.toString());
+        }
     }
 
-    public static void checkArgument(boolean expression, String errorMessage) {
+    public static void checkArgument(boolean expression, String errorMessage, String operation) {
         if (!expression) {
+            logger.error("operation=, {}" +
+                    "msg={}",operation, errorMessage);
             throw new IllegalArgumentException(errorMessage);
         }
     }
