3a4397d9bdb2426dd208c246d2e537821b5a998a
Rubem Neto <rubemviscard.neto@blip.pt>
2025-10-07 16:53:11 +0100

    chore: requested changes

--- src/main/java/io/rubuy74/rhs/domain/Market.java
+++ src/main/java/io/rubuy74/rhs/domain/Market.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.rubuy74.rhs.utils.ValidatorUtils;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Market {
     @JsonProperty
@@ -16,9 +17,11 @@ public class Market {
     private List<Selection> selections;
 
     public Market(String id, String name, List<Selection> selections) {
-        ValidatorUtils.checkArgument(id == null,        "Market id is null","create_event");
-        ValidatorUtils.checkArgument(name == null,      "Market name is null","create_event");
-        ValidatorUtils.checkArgument(selections == null,"Market date is null","create_event");
+        ValidatorUtils.checkArgument(id == null,        "Market id is null","create_market");
+        ValidatorUtils.checkArgument(name == null,      "Market name is null","create_market");
+        ValidatorUtils.checkArgument(selections == null,"Market date is null","create_market");
+        ValidatorUtils.checkArgument(Objects.equals(id, ""),   "Market id is empty","create_market");
+        ValidatorUtils.checkArgument(Objects.equals(name, ""), "Market name is empty","create_market");
         this.id = id;
         this.name = name;
         this.selections = new java.util.ArrayList<>(selections);
