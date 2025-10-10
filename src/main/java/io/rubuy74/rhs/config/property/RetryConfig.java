fa9aa1408a7562ef60e19a7246ad665aed037ff8
netor74 <rubemviscard.neto@flutteruki.com>
2025-10-10 10:07:48 +0100

    Revert "chore: AtomicInteger for RetryConfig & refactor error handling"
    This reverts commit 0fbd4860a4a63ebe10ecc1edc6a7506945d9c014.

--- src/main/java/io/rubuy74/rhs/config/property/RetryConfig.java
+++ src/main/java/io/rubuy74/rhs/config/property/RetryConfig.java
@@ -6,48 +6,46 @@ import org.springframework.jmx.export.annotation.ManagedAttribute;
 import org.springframework.jmx.export.annotation.ManagedResource;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 @Component
 @ManagedResource
 public class RetryConfig {
 
-    private AtomicInteger maxRetries;
-    private AtomicInteger backoffTimeMs;
-    private AtomicInteger timeoutMs;
+    private int maxRetries;
+    private int backoffTimeMs;
+    private int timeoutMs;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RetryConfig.class);
 
     @ManagedAttribute
     public int getMaxRetries() {
-        return maxRetries.get();
+        return maxRetries;
     }
 
     @ManagedAttribute
     public void setMaxRetries(int maxRetries) {
         LOGGER.debug("operation=set_max_retries, msg=Set max retries to {}", maxRetries);
-        this.maxRetries.set(maxRetries);
+        this.maxRetries = maxRetries;
     }
 
     @ManagedAttribute
     public int getBackoffTimeMs() {
-        return backoffTimeMs.get();
+        return backoffTimeMs;
     }
 
     @ManagedAttribute
     public void setBackoff(int backoff) {
         LOGGER.debug("operation=set_backoff, msg=Set Backoff limit to {}", backoff);
-        this.backoffTimeMs.set(backoff);
+        this.backoffTimeMs = backoff;
     }
     @ManagedAttribute
     public int getTimeoutMs() {
-        return timeoutMs.get();
+        return timeoutMs;
     }
 
     @ManagedAttribute
     public void setTimeout(int timeout) {
         LOGGER.debug("operation=set_timeout, msg=Set timeout to {}", timeout);
-        this.timeoutMs.set(timeout);
+        this.timeoutMs = timeout;
     }
 
 }
--- src/main/java/io/rubuy74/rhs/domain/Selection.java
+++ src/main/java/io/rubuy74/rhs/domain/Selection.java
@@ -2,7 +2,6 @@ package io.rubuy74.rhs.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.MoreObjects;
-import io.rubuy74.rhs.utils.ValidatorUtils;
 
 public class Selection {
     @JsonProperty
@@ -15,9 +14,15 @@ public class Selection {
     private Double odd;
 
     public Selection(String id, String name, Double odd) {
-        ValidatorUtils.checkArgument(id == null,    "Selection id is null","create_event");
-        ValidatorUtils.checkArgument(name == null,  "Selection name is null","create_event");
-        ValidatorUtils.checkArgument(odd == null,   "Selection odd is null","create_event");
+        if (id == null) {
+            throw new IllegalArgumentException("id is null");
+        }
+        if (name == null) {
+            throw new IllegalArgumentException("name is null");
+        }
+        if (odd == null) {
+            throw new IllegalArgumentException("odd is null");
+        }
         this.id = id;
         this.name = name;
         this.odd = odd;
