f176edfb9c09f76b7f55fcd0f7186a009bc92744
netor74 <rubemviscard.neto@flutteruki.com>
2025-10-10 10:07:48 +0100

    Revert "chore: minor typos and timeout configurable value"
    This reverts commit 515350c1ba85d514b8d79ab3487b4b2e08b26fe8.

--- src/main/java/io/rubuy74/rhs/utils/ValidatorUtils.java
+++ src/main/java/io/rubuy74/rhs/utils/ValidatorUtils.java
@@ -19,10 +19,4 @@ public class ValidatorUtils {
         });
         return messages;
     }
-
-    public static void checkArgument(boolean expression, String errorMessage) {
-        if (!expression) {
-            throw new IllegalArgumentException(errorMessage);
-        }
-    }
 }
